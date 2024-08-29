27c15c7e9dd9279c22e8dbb3249f7859ff0975c9
Kauê Damasceno Farias <kauedamasfarias@gmail.com>
2024-08-29 09:43:57 -0300

    fisrt commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className='bg-red-400 w-full h-full flex justify-center items-center'>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+     </div>
+  )
+}
