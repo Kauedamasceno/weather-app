87bc409387b2884822adecaadf8bb79fcc597537
Kauê Damasceno Farias <kauedamasfarias@gmail.com>
2024-09-02 09:10:25 -0300

    Hook is made up

--- src/hooks/useFetchCurrent.ts
+++ src/hooks/useFetchCurrent.ts
@@ -1,9 +1,16 @@
+ type useSearchWeatherProps = {
+  search: String
+ }
 
-export const useSearchWeather = async (props: String) => {
+export const useSearchWeather = async ({search}:useSearchWeatherProps) => {
+  const SearchSplit = search.split(" ")
+  console.log(SearchSplit)
   const searchKey = import.meta.env.VITE_KEY_WEATHER_API
   console.log(searchKey)
-  console.log(props)
-  const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${props}&appid=${searchKey}&units=metric&lang=pt_br`)
+  console.log(search)
+  const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${searchKey}&units=metric`)
   const response = await data.json()
   return console.log(response)
 }
+
+
