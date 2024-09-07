5a1ef11f17f376c46bbd87809862b2ac66cb0a11
Kauê Damasceno Farias <kauedamasfarias@gmail.com>
2024-09-07 19:36:29 -0300

    fix a nug

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import Logo from './components/Logo';
-import Search from './components/Search';
 import  MainDisplay  from './components/DisplayMain';
-import DisplayDetails from './components/DisplayDetails';
 import DisplayContainerDetails from './template/DisplayContainerDetails';
-
 export default function App() {
    
    return <div className=' w-full h-full justify-between flex max-md:flex-col bg-[url("./assets/background2.jpg")] bg-cover text-white max-sm:h-full max-sm:w-full '>
