f25f1de186be64dd3a8d0d2d8c5e50fd92ecc6dd
Kauê Damasceno Farias <kauedamasfarias@gmail.com>
2024-12-11 11:40:41 -0300

    removed error search

--- src/hooks/useFetchCurrent.ts
+++ src/hooks/useFetchCurrent.ts
@@ -7,8 +7,6 @@ export const useSearchWeather = async ({
 }: useSearchWeatherProps) => {
   const searchKey = import.meta.env.VITE_KEY_WEATHER_API;
 
-  searchInput = WordInput(searchInput);
-
   const data = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${searchKey}&units=metric&lang=pt_br`,
   );
@@ -19,27 +17,3 @@ export const useSearchWeather = async ({
   const response = await data.json();
   return response;
 };
-
-/////////////////////////////////////////////////////////////////////////////////////////
-//       remove blank space
-export const WordInput = (word: String) => {
-  let wordArray = word.split(" ");
-  if (wordArray[1] === "") {
-    const WordSlice = wordArray.find((e) => e !== "");
-    if (WordSlice) word = WordSlice;
-    return word;
-  }
-  return word;
-};
-
-// //const WordInput = (word:String) => {
-//   let wordArray = word.split(' ')
-//   console.log(wordArray)
-//   if (wordArray[wordArray.length - 1] === '') {
-//     const NumberLast = wordArray.lastIndexOf(" ")
-//     word = wordArray.slice(0,NumberLast).join(' ')
-//     console.log(word)
-//   return word
-//   }
-
-//   return word
