d476ccb452b935cfe7f47549966c0def961b6e0f
Kauê Damasceno Farias <kauedamasfarias@gmail.com>
2024-12-11 11:35:05 -0300

    removed one more error eslint

--- src/__tests__/useFetchLoading.test.tsx
+++ src/__tests__/useFetchLoading.test.tsx
@@ -1,31 +1,8 @@
 import { useLoadFetch } from "../hooks/useFetchLoading";
 import { useSearchWeather } from "../hooks/useFetchCurrent";
-import { describe, expect, it, Mock, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { renderHook } from "@testing-library/react";
 
-const data = {
-  weather: [
-    {
-      id: 0,
-      main: "",
-      description: "",
-      icon: "",
-    },
-  ],
-  main: {
-    temp: 0,
-    temp_min: 0,
-    temp_max: 0,
-    humidity: 0,
-  },
-  wind: {
-    speed: 0,
-  },
-  clouds: {
-    all: 0,
-  },
-  name: "",
-};
 // // Mock do hook `useSearchWeather`
 vi.mock("../hooks/useFetchCurrent", () => ({
   useSearchWeather: vi.fn(),
